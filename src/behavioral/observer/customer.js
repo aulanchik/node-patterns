145cb63ec6ba99112e4976a03e190b025064cdab
Artyom Ulanchik <artyom.u@outlook.com>
2021-10-11 23:51:17 +0100

    observer pattern

---
+++ src/behavioral/observer/customer.js
@@ -0,0 +1,12 @@
+class Customer {
+
+    constructor(name) {
+        this.name = name;
+    }
+
+    notify(storeName, discount) {
+        console.log(`${this.name}, there is a sale at ${storeName}! ${discount}% off everything!`);
+    }
+}
+
+module.exports = Customer;
